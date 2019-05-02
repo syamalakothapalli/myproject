60e2ba0d91aa09519f060333f0e49d90c41c87e8
syamalakothapalli <50200312+syamalakothapalli@users.noreply.github.com>
2019-05-02 17:07:30 +0530

    Update sample3.py

--- sample3.py
+++ sample3.py
@@ -1,46 +1,32 @@
 l1,p = list(input()),1
 while l1 != ['Z']:
-    l2 = input()
-    l3 = input()
-    l4 = input()
-    l5 = input()
-    list1 = [list(l1),list(l2),list(l3),list(l4),list(l5)]
-    list2 = list(input())
+    l2,l3,l4,l5 = input(),input(),input(),input()
+    list1,list2 = [list(l1),list(l2),list(l3),list(l4),list(l5)],list(input())
     print('puzzle #',p)
     for i in range(5):
         for j in range(5):
-            if list1[i][j] == " ":
-                f,s = i,j
+            if list1[i][j] == " ":f,s = i,j
     for ch in range(len(list2)):
         if list2[ch] == 'A':
             if f == 0:
                 print('This configuration is not possible')
                 break
-            else:    
-                list1[f][s],list1[f-1][s] = list1[f-1][s],list1[f][s]
-                f,s = f-1,s
-
+            else:list1[f][s],list1[f-1][s],f,s = list1[f-1][s],list1[f][s],f-1,s
         elif list2[ch] == 'B':
             if f == 5:
                 print('This configuration is not possible')
                 break
-            else:    
-                list1[f][s],list1[f+1][s] = list1[f+1][s],list1[f][s]
-                f,s = f+1,s
+            else:list1[f][s],list1[f+1][s]f,s = list1[f+1][s],list1[f][s],f+1,s
         elif list2[ch] == 'R':
             if s == 5:
                 print('This configuratiom is not possible')
                 break
-            else:
-                list1[f][s],list1[f][s+1] = list1[f][s+1],list1[f][s]
-                f,s = f,s+1
+            else:list1[f][s],list1[f][s+1],f,s = list1[f][s+1],list1[f][s],f,s+1
         elif list2[ch] == 'L':
             if s == 0:
                 print('This configuration is not possible')
                 break
-            else:
-                list1[f][s],list1[f][s-1] = list1[f][s-1],list1[f][s]
-                f,s = f,s-1
+            else:list1[f][s],list1[f][s-1],f,s = list1[f][s-1],list1[f][s],f,s-1
         elif list2[ch] == '0':
             for f in range(5):
                 for s in range(5):
